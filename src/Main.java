1df59242e2c7b66bcc5ab9f724edf259cfca160b
2019014266 <iraira7777@naver.com>
2020-12-07 17:28:52 +0900

    CLI VERSION(PACKAGE TRIMMED)

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+//package com.company;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        //Set Database Connection
+        Connection connection = DatabaseHandler.getConnect();
+
+        //Intro
+        System.out.println("******* Welcome to Kyum's Music Streaming Service! *******\n");
+
+        //Input from user - get instruction
+        int instr = -1;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n- MAIN PAGE -");
+            System.out.print("0. EXIT\n1.SIGN IN\n2.REGISTER\n3.ADMINISTRATOR MENU\nYOUR OPTION: ");
+            instr = Integer.parseInt(sc.next());
+
+            switch (instr) {
+                case 0 -> {
+                    System.out.println("Closing...");
+                    connection.close();
+                    exit(0);
+                }
+                case 1 -> UserFunction.signIn(connection);
+                case 2 -> UserFunction.register(connection);
+                case 3 -> AdminFunc.AdminAuth(connection);
+                default -> {
+                    System.out.println("ERROR: Invalid Input");
+                }
+            }
+        }
+    }
+}
+
---
+++ src/StringUtils.java
@@ -0,0 +1,18 @@
+public class StringUtils {
+    public static int parse(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Input Error!: Press Only Number.");
+        }
+        return -1;
+    }
+
+    public static String stringTrim(String input){
+        return stringReplace(input).trim();
+    }
+
+    private static String stringReplace(String input){
+        return input.replace("'","").replace("\"","");
+    }
+}
